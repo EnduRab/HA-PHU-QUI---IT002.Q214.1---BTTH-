f10d5d9ef3191aa21b142c1158f032665be786bc
EnduRab <111481979+EnduRab@users.noreply.github.com>
2026-03-19 00:04:04 +0700

    Add files via upload

--- 25521535_BTTH1/Bai1.cpp
+++ 25521535_BTTH1/Bai1.cpp
@@ -1,33 +1,43 @@
 #include <iostream>
-#include <climits>
+#include <limits>
+#include <cmath>
 using namespace std;
 
 struct PhanSo {
+
     int tu;
     int mau;
 
+// Khởi tạo phân số, mặc định là 0/1 nếu không truyền tham số
     PhanSo(int t = 0, int m = 1){
         tu = t;
         mau = m;
         RutGon();
     }
-
+// Nhập vào 2 số nguyên a và b và trả ước chung lớn nhất của 2 số
     int gcd(int a,int b) {
         if(b == 0) return a;
         return gcd(b, a % b);
     }
-
+// Nhập dữ liệu tử số và mẫu số, gán giá trị cho biến 'tu' va 'mau', tự động kiểm tra tính hợp lệ (nếu không hợp lệ sẽ bắt nhập lại) và gọi hàm rút gọn phân số
     void Nhap() {
-        cout << "Nhap tu va mau cua phan so (cach nhau bang dau cach): ";
-        if(!(cin >> tu >> mau)) {
-            CheckInt();
+        while (true) {
+            cout << "Nhap tu va mau cua phan so (cach nhau bang dau cach): ";
+            if (cin >> tu >> mau) {
+                if (mau != 0) {
+                    break; 
+                } else {
+                    cout << "Loi: Mau so phai khac 0!" << endl;
+                }
+            } else {
+                cout << "Loi: Vui long chi nhap so nguyen!" << endl;
+                cin.clear(); 
+                cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
+            }
         }
-        if(mau == 0) {
-            MauKhacKhong();
-        }
-        RutGon();
+        RutGon(); 
     }
-
+//
     void RutGon() {
         int ucln = gcd(abs(tu),abs(mau));
         tu /= ucln;
@@ -37,7 +47,7 @@ struct PhanSo {
             mau = -mau;
         }
     }
-
+//
     void Xuat() {
         if(mau == 1) {
             cout << tu << "\n";
@@ -45,21 +55,6 @@ struct PhanSo {
         else {
             cout << tu << "/" << mau << "\n";
         }
-    }
-
-    void MauKhacKhong() {
-        while(mau == 0)
-            {
-                cout << "Vui long nhap mau khac 0" << "\n";
-                Nhap();
-            }
-    }
-
-    void CheckInt() {
-        cout << "Vui long nhap mot so nguyen: " << "\n";
-        cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n'); 
-        Nhap();
     }
 };
 
